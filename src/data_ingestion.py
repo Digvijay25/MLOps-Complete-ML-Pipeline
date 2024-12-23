ef2ea4a8f9be2520400367bc37696079780ea168
Digvijay Singh <digvijay2560@gmail.com>
2024-12-23 18:24:33 +0000

    params configured for the pipeline

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os 
+import yaml
 import logging
 from sklearn.model_selection import train_test_split
 
@@ -24,6 +25,24 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 
 def load_data(data_path: str) -> pd.DataFrame:
@@ -71,7 +90,9 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        test_size = 0.2
+        # test_size = 0.2
+        params = load_params('params.yaml')
+        test_size = params['data_ingestion']['test_size']
         df = load_data('experiments\spam.csv')
         final_df = preprocess_data(df)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=42)
